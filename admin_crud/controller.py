cf407bf27aa61afdb9b234641a192cc8307c15dd
Dmitry Prekrasnov <dima@smoothie.digital>
2017-09-08 00:09:41 +0300

    Add detail action

--- admin_crud/controller.py
+++ admin_crud/controller.py
@@ -8,11 +8,13 @@ class AdminController(object):
     def get_actions(self):
         return {
             'list': self.list,
+            'detail': self.detail,
         }
 
-    def get_template_name(self, action):
+    def get_template_names(self, action):
         template_names = {
-            'list': 'admin_crud/list.html'
+            'list': 'admin_crud/list.html',
+            'detail': 'admin_crud/detail.html',
         }
 
         return template_names[action]
@@ -22,7 +24,12 @@ class AdminController(object):
         return data
     
     def list(self, request, *args, **kwargs):
-        template = self.get_template_name('list')
+        template = self.get_template_names('list')
+        context = self.get_context_data()
+        return TemplateResponse(request, template, context)
+
+    def detail(self, request, *args, **kwargs):
+        template = self.get_template_names('list')
         context = self.get_context_data()
         return TemplateResponse(request, template, context)
 
@@ -31,5 +38,6 @@ class AdminController(object):
         Generate urls for any available actions
         """
         return [
-            url(r'^$', self.list)
+            url(r'^$', self.list),
+            url(r'^(?<pk>\d+)/$', self.detail),
         ]
